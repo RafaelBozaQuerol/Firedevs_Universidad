2d59f423a3b60f443811df7f643fc487bd2c0f6b
RafaelBozaQuerol <89703232+RafaelBozaQuerol@users.noreply.github.com>
2021-08-28 13:55:43 -0400

    Add files via upload

---
+++ universidad/static/universidad/scripts/main.js
@@ -0,0 +1,20 @@
+﻿Objeto = {
+
+    __init__: function (id, modelo, ventana_id) {
+        Objeto.id = id;
+        Objeto.modelo = modelo;
+        Objeto.ventana_id = ventana_id;
+        Objeto.url_base = "/" + modelo + "/" + id;
+        return this
+    },
+
+    eliminar: function () {
+        $("#" + Objeto.ventana_id).find("a").attr('href', Objeto.url_base + "/eliminar");
+    },
+
+}
+
+$(document).on('ready', function() {
+
+
+});
